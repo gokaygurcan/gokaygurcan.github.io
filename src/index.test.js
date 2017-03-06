b8c93f3a3c92ee4e67a23d46fae461e1c24010bc
Gökay Gürcan <gokaygurcan@gmail.com>
2017-03-07 00:52:11 +0300

    chore: Initialize Jest

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+// @flow
+
+import {name} from '../package';
+
+// flow-disable-next-line
+test('pkg > name', () => {
+  // flow-disable-next-line
+  expect(name).toBe('gokaygurcan.github.io');
+});
