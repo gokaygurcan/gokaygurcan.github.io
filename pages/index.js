e2ef9192d869a991fa8e64a8455407d1320b25b2
Gökay Gürcan <gokaygurcan@gmail.com>
2017-04-24 14:55:06 +0300

    feature: Add React, ReactDOM and Next.js packages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// gokaygurcan.github.io
+//
+// https://www.gokaygurcan.com/
+// MIT © Gökay Gürcan
+
+const index = () => (
+  <div>
+    gokaygurcan.github.io
+  </div>
+);
+
+export default index;
